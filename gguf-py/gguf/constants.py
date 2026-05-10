bc9f2aa91d8be6e950b9ec35a1203a297a9230a1
Michael Wand <michael@baybridgeaquarium.com>
2026-05-10 00:18:56 -0700

    mxfp6: add CPU E2M3 support (rebased)

--- gguf-py/gguf/constants.py
+++ gguf-py/gguf/constants.py
@@ -4711,7 +4711,8 @@ class GGMLQuantizationType(IntEnum):
     MXFP4   = 39
     NVFP4   = 40
     Q1_0    = 41
-    Q2_0    = 42
+    Q2_0        = 42
+    MXFP6_E2M3 = 50
 
 
 class ExpertGatingFuncType(IntEnum):
@@ -4768,6 +4769,7 @@ class LlamaFileType(IntEnum):
     MOSTLY_NVFP4         = 39  # except 1d tensors
     MOSTLY_Q1_0          = 40  # except 1d tensors
     MOSTLY_Q2_0          = 41  # except 1d tensors
+    MOSTLY_MXFP6_E2M3    = 50  # except 1d tensors
 
     GUESSED              = 1024  # not specified in the model file
 
@@ -4896,6 +4898,7 @@ GGML_QUANT_SIZES: dict[GGMLQuantizationType, tuple[int, int]] = {
     GGMLQuantizationType.NVFP4:   (64, 4 + 32),
     GGMLQuantizationType.Q1_0:    (128, 2 + 16),
     GGMLQuantizationType.Q2_0:    (64, 2 + 16),
+    GGMLQuantizationType.MXFP6_E2M3:   (64, 52),
 }
 
 
